90297fa2c5581f9c648cc076f9ce50845d3b089e
Craig Jenkins <mccraigj@gmail.com>
2018-03-19 17:24:47 +0000

    Navigation updates

--- CJ.IdentityServer/Controllers/HomeController.cs
+++ CJ.IdentityServer/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace CJ.IdentityServer.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+  public class HomeController : Controller
+  {    
+    private readonly IConfigurationSection _appSettings;
+
+    public HomeController(IConfiguration configuration)
+    {      
+      _appSettings = configuration.GetSection("AppSettings");
     }
+
+    public IActionResult Index()
+    {
+      var defaultHomePage = _appSettings["DefaultHomePage"];
+      if (!string.IsNullOrEmpty(defaultHomePage))
+      {
+        return Redirect(defaultHomePage);
+      }
+      return View();
+    }
+  }
 }
